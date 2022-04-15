f2d5283febe1fc9459e929d69ae5cf4fd829095e
zackynson <crys.chb@hotmail.com>
2022-04-15 18:18:27 -0300

    fix: update customer controller responses

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,28 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 
 import { makeCustomerRegisterController } from '@/main/factories';
 import { makeCustomerFindByIdController } from '@/main/factories/customers/controllers/customer-find-by-id-controller-factory';
+import { Response } from 'express';
 
 @Controller('customers')
 export class AppController {
   @Post()
-  create(@Body() body: any) {
-    return makeCustomerRegisterController().handle({
+  async create(@Body() body: any, @Res() res: Response) {
+    const response = await makeCustomerRegisterController().handle({
       body,
     });
+
+    return res.status(response.statusCode).json(response);
   }
 
   @Get(':id')
-  findById(@Param('id') id: string) {
-    return makeCustomerFindByIdController().handle({
+  async findById(@Param('id') id: string, @Res() res: Response) {
+    const response = await makeCustomerFindByIdController().handle({
       params: {
         id,
       },
     });
+
+    return res.status(response.statusCode).json(response);
   }
 }
