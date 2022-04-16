0b9573ddc01e11a85fa2ddcca67c94eb674399a4
zackynson <crys.chb@hotmail.com>
2022-04-16 15:00:57 -0300

    feat: add customer update route

--- tests/app.controller.spec.ts
+++ tests/app.controller.spec.ts
@@ -32,7 +32,7 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('@POST -> customers', () => {
+  describe('@POST -> /customers', () => {
     it('should create and return a new Customer', async () => {
       const res = httpMocks.createResponse() as Response;
       let resBody: any;
@@ -53,6 +53,87 @@ describe('AppController', () => {
       expect(resBody.body.document).toEqual(123456789);
       expect(resBody.body.name).toEqual('any_name');
       expect(resBody.body.id).toBeTruthy();
+    });
+  });
+
+  describe('@GET -> /customers/:id', () => {
+    it('should create and return a new Customer', async () => {
+      const res = httpMocks.createResponse() as Response;
+      let createResponseSpy: any;
+      let findResponseSpy: any;
+
+      jest
+        .spyOn(res, 'json')
+        .mockImplementationOnce((data) => (createResponseSpy = data));
+
+      const createCustomerResponse = await appController.create(
+        {
+          name: 'any_name',
+          document: 123456789,
+        },
+        res,
+      );
+
+      jest
+        .spyOn(res, 'json')
+        .mockImplementationOnce((data) => (findResponseSpy = data));
+
+      expect(createCustomerResponse.statusCode).toBe(201);
+      expect(createResponseSpy.body.id).toBeTruthy();
+
+      const findCustomerResponse = await appController.findById(
+        createResponseSpy.body.id,
+        res,
+      );
+
+      expect(findCustomerResponse.statusCode).toBe(200);
+      expect(createResponseSpy.body).toEqual(findResponseSpy.body);
+    });
+  });
+
+  describe('@PUT -> /customers/:id', () => {
+    it('should create and return a new Customer', async () => {
+      const res = httpMocks.createResponse() as Response;
+      let createResponseSpy: any;
+      let updateResponseSpy: any;
+
+      jest
+        .spyOn(res, 'json')
+        .mockImplementationOnce((data) => (createResponseSpy = data));
+
+      const createCustomerResponse = await appController.create(
+        {
+          name: 'any_name',
+          document: 123456789,
+        },
+        res,
+      );
+
+      jest
+        .spyOn(res, 'json')
+        .mockImplementationOnce((data) => (updateResponseSpy = data));
+
+      expect(createCustomerResponse.statusCode).toBe(201);
+      expect(createResponseSpy.body.id).toBeTruthy();
+
+      const updateData = {
+        document: 123456,
+        name: 'new_name',
+      };
+      const updateCustomerResponse = await appController.updateOne(
+        createResponseSpy.body.id,
+        updateData,
+        res,
+      );
+
+      expect(createResponseSpy.body.document).toEqual(123456789);
+      expect(createResponseSpy.body.name).toEqual('any_name');
+      expect(createResponseSpy.body.id).toBeTruthy();
+
+      expect(updateCustomerResponse.statusCode).toBe(200);
+      expect(updateResponseSpy.body.id).toEqual(updateResponseSpy.body.id);
+      expect(updateResponseSpy.body.name).toEqual(updateData.name);
+      expect(updateResponseSpy.body.document).toEqual(updateData.document);
     });
   });
 });
