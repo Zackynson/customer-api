6bb9b972e781ff1a56f8ce29396f36c5acab6254
zackynson <crys.chb@hotmail.com>
2022-04-19 20:43:57 -0300

    chore: set default port to 3000

--- src/layers/main/config/env.ts
+++ src/layers/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 3000,
   ssoClientId: process.env.SSO_CLIENT_ID,
   ssoClientSecret: process.env.SSO_CLIENT_SECRET,
 };
